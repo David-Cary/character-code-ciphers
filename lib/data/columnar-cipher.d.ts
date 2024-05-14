b2339cdfbdbf873606814b2de399da4b1499ee5a
David Cary <shimeran@tutanota.com>
2024-05-13 20:55:38 -0400

    Adding encoders and tests

--- lib/data/columnar-cipher.d.ts
+++
@@ -1,9 +0,0 @@
-import { type StringEncoder } from './encoders';
-export declare class ColumnarCipher implements StringEncoder {
-    spliceIndices: number[];
-    constructor(spliceIndices?: number[]);
-    encode(source: string): string;
-    getCharacterGrid(source: string): string[];
-    shuffleText(source: string[]): string[];
-    decode(source: string): string;
-}
